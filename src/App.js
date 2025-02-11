33d6b4181e823024df398db49ad030d4f05481d3
Gian1500 <gianuniversidad@gmail.com>
2025-02-11 01:57:15 -0300

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="flex w-full h-screen">
+      <div className='w-full flex items-center justify-center lg:w1/2'>
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
